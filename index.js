5b14ba7d646e1420ff47469049f5b048a859e60d
Desiree Lerma <desi.lerma96@yahoo.com>
2021-02-08 02:06:46 -0600

    commit tree fa778750f2f01a096a8430dbc2d8e4a0ed546772

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 62
+const num2 = 1
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 1000 ) + 1;
+
+const num3 = 12
+const num4 = 3
+
+const mod = num3 / num4
+
+const max = Math.max(1, 5, 7, 13, 17, 20);
